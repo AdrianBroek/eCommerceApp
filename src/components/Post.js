e54b29e69a525657f02aec2021a7dee422ebe289
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2023-05-26 20:29:40 +0200

    blog kinda finished

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PostAction from "../actions/PostAction";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import noPicture from '../images/no-picture.jpg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag, faX, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Post = () => {
     const dispatch = useDispatch()
     const {activePost} = useSelector(state => state.blog)
     const {pathname} = useLocation()
+    const navigate = useNavigate()
 
     function getPostIdFromUrl(){
         const link = pathname.substring(pathname.lastIndexOf("/"))
         const linkDone = link.replace("/", '')
-        console.log(linkDone)
         dispatch(PostAction(linkDone))
     }
 
@@ -22,18 +25,25 @@ const Post = () => {
     return (
         <>
         {activePost ? 
-        <article id="post" className={'vol flex '+activePost.id}>
-            <div>
-                <img />
+        <article id="post" className={'vol flex ' + activePost.id}>
+            <div className="exit-post"
+            onClick={()=> navigate(-1)}>
+                <FontAwesomeIcon size="xl" icon={faXmarkCircle}/>
             </div>
-            <div>
+            <div className="post-pic">
+                <img src={noPicture} alt="no picture"/>
+            </div>
+            <div className="title">
                 <h2>{activePost.title}</h2>
             </div>
-            <div>
+            <div className="body">
                 <p>{activePost.body}</p>
             </div>
-            <div>
-                <p>{activePost.body}</p>
+            <div className="tags flex">
+                <FontAwesomeIcon icon={faHashtag}/>
+                {activePost.tags.map((tag)=>(
+                    <p>{tag}</p>
+                ))}
             </div>
         </article>
          : "" }
