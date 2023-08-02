e127add9a9960b59197cd91cfebf768452a7455f
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2023-08-02 20:15:12 +0200

    setting private to false

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index'
 import {BrowserRouter} from 'react-router-dom'
-// Apollo
-import {ApolloProvider} from '@apollo/react-hooks'
-import ApolloClient from 'apollo-boost'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -17,21 +14,13 @@ const store = createStore(
     composeEnhancers(applyMiddleware(thunk))
 )
 
-const client = new ApolloClient({
-  uri: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbguyu910qs001t16nfw3ixe/master',
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-
-
     <Provider store={store}>
-    <ApolloProvider client={client}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
       </BrowserRouter>
-      </ApolloProvider>
     </Provider>
 
 );
