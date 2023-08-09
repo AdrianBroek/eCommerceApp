5f6187d7a0fa8e068a30c6a4cee966376f79a358
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2023-08-09 22:05:44 +0200

    idk how to fix it tbh for now i am leaving refreshing error on react router, it doesnt really matter anyway its just a project

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
       </BrowserRouter>
     </Provider>
